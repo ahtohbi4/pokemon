cbd8258c9d0c546ea3038aa8d8c019c2eb08ea9c
Alexander Antonov <alexandr-post@yandex.ru>
2019-01-17 13:43:47 +0300

    API_BASE_URI was updated

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URI = 'http://pokeapi.salestock.net/api/v2/';
+export const API_BASE_URI = 'https://pokeapi.co/api/v2/';
 
 export const API_GET_POKEMONS_LIST = 'pokemon/';
 export const API_GET_POKEMON = 'pokemon/{id}/';
