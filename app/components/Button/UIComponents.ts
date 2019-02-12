8f8add65354d5e76222af4714782a21f8e817adb
Alexander Antonov <alexandr-post@yandex.ru>
2019-02-12 19:10:10 +0300

    The component Button was updated

--- app/components/Button/UIComponents.ts
+++ app/components/Button/UIComponents.ts
@@ -1,13 +1,31 @@
 import styled from '@Utils/styled';
 
 export const Container = styled.button`
-    background-color: blue;
+    padding: .5rem 1rem;
+
+    background-color: ${({ theme }) => theme.colors.button.normal};
     border: 0;
+    border-radius: 3px;
+
+    color: #fff;
+    font-family: ${({ theme }) => theme.font.family.primary};
+    font-size: 1rem;
+    line-height: 1.125;
 
     cursor: pointer;
 
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.button.hovered};
+    }
+
+    &:active {
+        background-color: ${({ theme }) => theme.colors.button.active};
+    }
+
     &:disabled {
-        background-color: #ccc;
+        background-color: ${({ theme }) => theme.colors.button.disabled};
+
+        color: #555;
 
         cursor: not-allowed;
     }
--- app/utils/styled/constants.ts
+++ app/utils/styled/constants.ts
@@ -1,8 +1,23 @@
 export const DEFAULT_THEME = {
     colors: {
+        blue300: '#4fc3f7',
+        blue400: '#29b6f6',
+        blue500: '#03a9f4',
+        blue600: '#039be5',
+        blue700: '#0288d1',
+        blue800: '#0277bd',
+
+        button: {
+            active: '#0277bd',
+            disabled: '#c4c5c6',
+            hovered: '#039be5',
+            normal: '#0288d1',
+        },
+
         link: {
-            normal: '#0496ff',
-            hovered: '#0489e8',
+            hovered: '#03a9f4',
+            normal: '#039be5',
+            visited: '#586069',
         },
 
         text: {
